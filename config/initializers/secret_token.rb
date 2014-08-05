47074c219aa5ff9a065b4cd2f8716950712c4fe4
stevievines <stevievines@gmail.com>
2014-08-04 20:41:07 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Syncopy::Application.config.secret_token = 'e6f6f2a24d9a27e91b0f1acf08c04407d1e0de2507d42d73abf3c089800b50aab282433972372478c0c657739441d5791d3821679d584996dbf285f2b1500004'
